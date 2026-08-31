820d09c21c27f085ce4d2a15e50df637fc7fb92c
John Paul Miraflores <miraflores.john@gmail.com>
2026-08-31 21:26:09 +0800

    Update Ztk formula to v1.2.3 with corresponding SHA256 checksum

--- Formula/ztk.rb
+++ Formula/ztk.rb
@@ -1,21 +1,30 @@
 class Ztk < Formula
-  ZTK_VERSION = "1.2.2".freeze
+  ZTK_VERSION = "1.2.3".freeze
 
   desc "DevSecOps bootstrap toolkit for cloud, containers, Kubernetes, and security"
   homepage "https://github.com/Daxxtropezz/ZeroTrustKit"
   url "https://github.com/Daxxtropezz/ZeroTrustKit/archive/refs/tags/v#{ZTK_VERSION}.tar.gz"
-  sha256 "6824c4034fc5e76aad1b1bd4c964ec1a81599ec7cdf6047b83abe90221c473c8"
+  sha256 "22d915bc33dfc0be19e1fc52c54bff61615d35c1e0bb2faedbade1f86fb498e1"
   license "MIT"
 
   depends_on "bash"
 
   def install
+    inreplace "ztk",
+              "#!/usr/bin/env bash",
+              "#!#{formula_opt_bin("bash")}/bash"
+
     bin.install "ztk"
   end
 
   test do
     output = shell_output("#{bin}/ztk --version")
     assert_match "ZeroTrustKit v#{version}", output
+
+    output = shell_output("#{bin}/ztk --list")
+    assert_match "Base Packages", output
+    assert_match "Git - Version Control", output
+    assert_match "Azure CLI - Microsoft Azure", output
 
     assert_match "ZeroTrustKit", shell_output("#{bin}/ztk --help")
   end
