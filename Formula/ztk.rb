3ccc59e21bc785d7c00553a186aa2b3f6c602501
John Paul Miraflores <miraflores.john@gmail.com>
2026-08-31 14:19:50 +0800

    Refactor Ztk formula to use version constant for URL and assertions

--- Formula/ztk.rb
+++ Formula/ztk.rb
@@ -1,9 +1,12 @@
 class Ztk < Formula
+  ZTK_VERSION = "1.2.1".freeze
+
   desc "DevSecOps bootstrap toolkit for cloud, containers, Kubernetes, and security"
   homepage "https://github.com/Daxxtropezz/ZeroTrustKit"
-  url "https://github.com/Daxxtropezz/ZeroTrustKit/archive/refs/tags/v1.2.1.tar.gz"
+  url "https://github.com/Daxxtropezz/ZeroTrustKit/archive/refs/tags/v#{ZTK_VERSION}.tar.gz"
   sha256 "d9d421fce91d80dd5d95da7498e95bd38c20ed30d2a22152d9bfd7a51aa43f09"
   license "MIT"
+  version ZTK_VERSION
 
   depends_on "bash"
 
@@ -12,7 +15,9 @@ class Ztk < Formula
   end
 
   test do
-    output = shell_output("#{bin}/ztk --help")
-    assert_match "ZeroTrustKit v1.2.1", output
+    output = shell_output("#{bin}/ztk --version")
+    assert_match "ZeroTrustKit v#{version}", output
+
+    assert_match "ZeroTrustKit", shell_output("#{bin}/ztk --help")
   end
 end
