abb182940ba3b5b1ff2d58b194d5eb284b45d274
John Paul Miraflores <miraflores.john@gmail.com>
2026-08-31 21:53:31 +0800

    Remove duplicate SHA256 checksum and unnecessary assertions from Ztk formula tests

--- Formula/ztk.rb
+++ Formula/ztk.rb
@@ -6,7 +6,6 @@ class Ztk < Formula
   homepage "https://github.com/Daxxtropezz/ZeroTrustKit"
   url "https://github.com/Daxxtropezz/ZeroTrustKit/archive/refs/tags/v#{ZTK_VERSION}.tar.gz"
   sha256 "22d915bc33dfc0be19e1fc52c54bff61615d35c1e0bb2faedbade1f86fb498e1"
-  sha256 "22d915bc33dfc0be19e1fc52c54bff61615d35c1e0bb2faedbade1f86fb498e1"
   license "MIT"
 
   depends_on "bash"
@@ -22,11 +21,6 @@ class Ztk < Formula
   test do
     output = shell_output("#{bin}/ztk --version")
     assert_match "ZeroTrustKit v#{version}", output
-
-    output = shell_output("#{bin}/ztk --list")
-    assert_match "Base Packages", output
-    assert_match "Git - Version Control", output
-    assert_match "Azure CLI - Microsoft Azure", output
 
     output = shell_output("#{bin}/ztk --list")
     assert_match "Base Packages", output
